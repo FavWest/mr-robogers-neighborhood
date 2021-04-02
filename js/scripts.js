d0eea17a85cf1897799a9ad0102881e6f27544cc
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-02 10:04:19 -0700

    make webpage accept user input, apply beepBoop(), and return result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business Logic
 function beepBoop(number) {
   let result=[];
   for(i=0; i<=number; i++){
@@ -14,23 +15,11 @@ function beepBoop(number) {
   return result;
 }
 
-//TESTS
-function displayResults(input, result){
-  console.log(input);
-  console.log(result);
-}
-console.log(beepBoop(0));
-console.log("[0]");
-console.log(beepBoop(15).length);
-console.log(16);
-console.log(beepBoop(50)[45]);
-console.log(45);
-console.log(beepBoop(4)[3]);
-console.log("Won't you be my neighbor?");
-console.log(beepBoop(4)[2]);
-console.log("Boop");
-displayResults(beepBoop(4)[1], "Beep!");
-displayResults(beepBoop(25)[23], "Won't you be my neighbor?");
-displayResults(beepBoop(25)[25], "Boop");
-displayResults(beepBoop(25)[19], "Beep!");
-displayResults(beepBoop(125)[123]+beepBoop(110)[105]+beepBoop(215)[210], "Won't you be my neighbor?Beep!Boop")
+//UI Logic
+$(document).ready(function() {
+  $("#number-form").submit(function(event) {
+    event.preventDefault();
+    const response=beepBoop($("#number").val());
+    $("#robo-response").text(response);
+  })
+})
