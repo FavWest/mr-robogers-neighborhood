22d4f6fcae7504d9a0caf5a175bab436d7d36366
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-02 08:32:38 -0700

    pass test: beepBoop function returns [0] for input 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function beepBoop(number) {
-  let result=[number]
+  let result=[number];
   return result;
 }
 
 //TESTS
-console.log("Expected: [0] \nResult:   "+ beepBoop(0));
+console.log(beepBoop(0));
 
